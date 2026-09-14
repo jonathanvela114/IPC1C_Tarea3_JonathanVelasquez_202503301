f8bbfe70adc27237db24e56d60f1d1afbd740c3d
Jonathan Velásquez <jonathanvela114@gmail.com>
2026-09-13 22:48:07 -0600

    Realización del main y verificación del programa

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,25 @@
 public class Main {
+    public static void main(String[] args){
+        Vehiculo[] guardado  = new Vehiculo[4];
+
+        guardado[0] = new Automovil("P123GTR", "Mazda", 340.0, 5);
+        guardado[1] = new Automovil("P234GTS", "Toyota", 360.0, 6);
+        guardado[2] = new Motocicleta("M456CBR", "Honda", 180, 600);
+        guardado[3] = new Motocicleta("M368ZHH", "Kawasaki", 200, 900);
+
+        double totalguardado = 0.0;
+
+        System.out.println("GUARDADO DE VEHÍCULOS");
+        System.out.println();
+
+        for (Vehiculo v : guardado){
+            double tarifa = v.calcularTarifa();
+            totalguardado += tarifa;
+            System.out.println(v.getPlaca() + " " + v.getMarca() + " Q" + tarifa);
+        }
+        System.out.println();
+        System.out.println("TOTAL DE DINERO: Q " +totalguardado);
+    }
 
 
 }
